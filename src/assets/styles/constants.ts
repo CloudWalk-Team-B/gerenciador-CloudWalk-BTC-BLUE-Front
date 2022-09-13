3be2a41811e1fc8c79bd16315c622111873e6cf9
Maurício L Santana Junior <68245295+LopezJunior@users.noreply.github.com>
2022-09-12 21:12:52 -0300

    Revert "Feat: config Styled-Components"

--- src/assets/styles/constants.ts
+++
@@ -1,6 +0,0 @@
-export const constants = {
-  bodyFontFamily: "Roboto",
-  bodyFontSize: "14px",
-  bodyLineHeight: "140%",
-  infoFontFamily: "Roboto",
-};
--- src/assets/styles/theme.ts
+++
@@ -1,9 +0,0 @@
-import { colors } from "./colors";
-import { constants } from "./constants";
-
-const theme = {
-  colors,
-  constants,
-} as const;
-
-export default theme;
