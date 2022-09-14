8e1be3cfb265ef07752c19c5bb12acc644fe6256
Maurício Júnior <456junior456@gmail.com>
2022-09-13 21:35:39 -0300

    Feat: Tela de produtos

---
+++ src/pages/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Content = styled.main`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  overflow: hidden;
+`;
