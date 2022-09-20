95d87c70d433984208bd9fbfbdf443e6d2c7c59b
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2022-09-19 22:34:38 -0300

    feat: context for product created

---
+++ src/contexts/product/index.tsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, ReactNode, useState, useEffect } from "react"
+import Api from "../../services/api";
+import { Product } from "../../types/interface";
+
+interface ProductsProviderProps {
+    children: ReactNode
+}
+
+interface ProductsProviderData{
+    products: Product[]
+    handleGetProduct: ()=> void
+}
+
+const ProductContext = createContext<ProductsProviderData>({} as ProductsProviderData);
+
+export const ProductsProvider = ({children}:ProductsProviderProps) => {
+
+    const token = localStorage.getItem("token")
+
+    const headers = {
+        headers: {
+            Authorization:`Bearer ${token}`
+        }
+    }
+
+    const [products, setProducts]= useState<Product[]>([]);
+
+    const handleGetProduct = ()=>{
+        Api.get("/product", headers).then(res=> {setProducts(res.data)})
+    }
+
+    useEffect(()=>handleGetProduct(),[])
+
+    return <ProductContext.Provider value={{ products, handleGetProduct }}>{children}</ProductContext.Provider>
+}
+
+export const useProducts = () => useContext(ProductContext)
