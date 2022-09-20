417c343f54012bbae8b8ef1f3897336a62c5ccb1
Maurício Júnior <456junior456@gmail.com>
2022-09-20 20:37:41 -0300

    Fix: erro deploy

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import * as S from "./style";
+import * as S from "./style.js";
 import InfoCard from "../../components/InfoCard";
 import LoginCard from "../../components/LoginCard";
 
