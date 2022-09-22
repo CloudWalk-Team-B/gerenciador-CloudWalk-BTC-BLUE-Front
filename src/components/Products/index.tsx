2ca6fb7a09ee8c8b0cded2921d7b2c989e14ebb0
Maurício Júnior <456junior456@gmail.com>
2022-09-21 21:01:51 -0300

    Feat: Info products Modal

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -5,36 +5,43 @@ import { useState } from "react";
 const ListProducts = () => {
   let list = [
     {
+      id: 1,
       name: "Racao",
       valor: 20,
       img: "https://tanakao.com.br/media/catalog/product/cache/d8d35210b0b8dd8c9dd34efdb8c87352/l/a/large_68d0c570-d74f-46ac-8151-8e8542e87e17_1k.png",
     },
     {
+      id: 2,
       name: "Coleira",
       valor: 50,
       img: "https://www.bellbold.com.br/image/cache/cache/1-1000/189/main/9b91-coleira-para-cachorro-poliester-estampa-bellbold-indigo-02-0-2-0-1-2-960x695.png",
     },
     {
+      id: 3,
       name: "Brinquedo",
       valor: 50,
       img: "https://amepettatix.vteximg.com.br/arquivos/ids/160534-1000-1000/Brinquedo_Pet_Total_Chinelo_De_174.jpg?v=637184071104400000",
     },
     {
+      id: 4,
       name: "Aranhador",
       valor: 50,
       img: "https://images.tcdn.com.br/img/img_prod/968088/brinquedo_arranhador_para_gato_com_cama_suspensa_306_1_0b78b2b42a76809e0e14c1d940be8248.png",
     },
     {
+      id: 5,
       name: "Shampoo",
       valor: 50,
       img: "http://www.crivialli.com.br/uploads/produtos/shampoo-doctor-animal-antipulgas-e-carrapaticida-350ml-1562694865.png",
     },
     {
+      id: 6,
       name: "Alpiste",
       valor: 50,
       img: "http://ponzan.com.br/wp-content/uploads/2018/10/ALPISTE.png",
     },
     {
+      id: 7,
       name: "Roupa",
       valor: 50,
       img: "https://d2r9epyceweg5n.cloudfront.net/stores/001/961/075/products/15100208427-whatsapp-image-2021-12-13-at-12-04-18-1-removebg-preview-17e808445236ea0ef116445408477807-640-0.png",
@@ -43,20 +50,34 @@ const ListProducts = () => {
 
   Modal.setAppElement("#root");
   const [modalIsOpen, setIsOpen] = useState(false);
-  const [product, setProduct] = useState([""]);
+  const [idProduct, setIdProduct] = useState(0);
 
-  function openModal(element: string[]) {
+  function openModal(element: any) {
     setIsOpen(true);
-    setProduct(element);
+    setIdProduct(element.id);
+    console.log(element.id);
   }
+
   function closeModal() {
     setIsOpen(false);
   }
 
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      border: "1px solid purple",
+    },
+  };
+
   return (
-    <>
+    <S.Container>
       <S.ProductsContainer>
-        {list.map<React.ReactNode>((element: any, index) => {
+        {list.map<React.ReactNode>((element, index) => {
           return (
             <S.CardProduct
               key={index}
@@ -77,17 +98,29 @@ const ListProducts = () => {
           );
         })}
       </S.ProductsContainer>
+
       <Modal
         isOpen={modalIsOpen}
-        // onAfterOpen={}
+        // onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
-        // style={}
+        style={customStyles}
         contentLabel="Example Modal"
       >
-        <h2>s </h2>
-        <button onClick={closeModal}>close</button>
+        <S.TitleModal>Infomações</S.TitleModal>
+        <S.InfoProductCard>
+          <S.CardImageProduct>
+            <S.ImageProduct src="http://ponzan.com.br/wp-content/uploads/2018/10/ALPISTE.png" />
+          </S.CardImageProduct>
+          <S.InfoProduct>
+            <h1>Nome: Pacote de Alpistes</h1>
+            <h1>Valor: R$15.00 </h1>
+            <h1>Categoria: Ração </h1>
+            <h1>Código: 197483209 </h1>
+            <h1>Disponivel: Sim </h1>
+          </S.InfoProduct>
+        </S.InfoProductCard>
       </Modal>
-    </>
+    </S.Container>
   );
 };
 
--- src/components/Products/style.ts
+++ src/components/Products/style.ts
@@ -1,5 +1,9 @@
 import styled, { css } from "styled-components";
 
+export const Container = styled.div`
+  ${({ theme }) => css``}
+`;
+
 export const ProductsContainer = styled.div`
   /* border: 1px solid black; */
   height: 87vh;
@@ -49,4 +53,50 @@ export const TextContainer = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
+`;
+
+export const TitleModal = styled.h2`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.primaryColor};
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50px;
+    height: 8vh;
+    font-weight: 300;
+    width: 40vw;
+    margin: 10px 10vw;
+    font-size: 7vh;
+  `}
+`;
+
+export const InfoProductCard = styled.div`
+  margin: 8vh 3vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const CardImageProduct = styled.span`
+  ${({ theme }) => css`
+    border-bottom: 1px solid ${theme.colors.primaryColor};
+    height: 30vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* border-radius: 10px; */
+  `}
+`;
+
+export const ImageProduct = styled.img`
+  height: 90%;
+  width: 90%;
+`;
+
+export const InfoProduct = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.primaryColor};
+    font-weight: 300;
+  `}
 `;
