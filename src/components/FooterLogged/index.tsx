ce12f9e13998d631b2d44ac701d67c0bad82043e
Maurício Júnior <456junior456@gmail.com>
2022-09-26 20:08:45 -0300

    Feat: props header

--- src/components/FooterLogged/index.tsx
+++ src/components/FooterLogged/index.tsx
@@ -4,39 +4,43 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { FiPlus } from "react-icons/fi";
 import { useAuth } from "../../contexts/auth";
 
-const FooterLogged = () => {
-
-  const { logout } = useAuth()
-  return (
-    <>
-      <S.Content className="animate__animated animate__slideInDown">
-        <S.BoxSoon>
-          <S.Soon
-            className="animate__animated animate__slideInLeft animate__delay-1s"
-            src={Logo} onClick={()=>{logout()}}
-          />
-          <S.Name className="animate__animated animate__bounceIn animate__delay-1s	">
-            Capivara <br />
-            Shop
-          </S.Name>
-        </S.BoxSoon>
-        <S.Search>
-          <S.TextSearch
-            className="animate__animated animate__jackInTheBox animate__delay-1s"
-            type="text"
-            placeholder="Procurar..."
-          ></S.TextSearch>
-        </S.Search>
-        <S.Update className="animate__animated animate__slideInRight animate__delay-1s">
-          <S.TextUpdate>
-            Atualização <br />
-            em massa
-          </S.TextUpdate>
-          <FiPlus color="white" fontSize="1.5em" />
-        </S.Update>
-      </S.Content>
-    </>
-  );
+const FooterLogged = (header: string) => {
+  const { logout } = useAuth();
+  if (header === "add") {
+    return (
+      <>
+        <S.Content className="animate__animated animate__slideInDown">
+          <S.BoxSoon>
+            <S.Soon
+              className="animate__animated animate__slideInLeft animate__delay-1s"
+              src={Logo}
+              onClick={() => {
+                logout();
+              }}
+            />
+            <S.Name className="animate__animated animate__bounceIn animate__delay-1s	">
+              Capivara <br />
+              Shop
+            </S.Name>
+          </S.BoxSoon>
+          <S.Search>
+            <S.TextSearch
+              className="animate__animated animate__jackInTheBox animate__delay-1s"
+              type="text"
+              placeholder="Procurar..."
+            ></S.TextSearch>
+          </S.Search>
+          <S.Update className="animate__animated animate__slideInRight animate__delay-1s">
+            <S.TextUpdate>
+              Atualização <br />
+              em massa
+            </S.TextUpdate>
+            <FiPlus color="white" fontSize="1.5em" />
+          </S.Update>
+        </S.Content>
+      </>
+    );
+  } else <h1>ola</h1>;
 };
 
 export default FooterLogged;
