52107ad00d87b024326c7ee9400ac360a45df819
Maurício Júnior <456junior456@gmail.com>
2022-11-10 10:49:53 -0300

    Fix: style modal new colab

--- src/components/ModalNewUser/style.ts
+++ src/components/ModalNewUser/style.ts
@@ -11,7 +11,7 @@ export const TitleComponent = styled.h2`
     height: 10%;
     font-weight: 300;
     width: 40vw;
-    margin: 18px 10vw;
+    margin: 18px 15vw;
     font-size: 3.5vh;
   `}
 `;
@@ -26,63 +26,61 @@ export const MainComponent = styled.div`
     padding: 5% 0;
     box-sizing: border-box;
 
-      div{
-        margin-top: 4vh;
-        display: flex;
-        flex-direction: row;
-        width: 60%;
-        justify-content: space-between;
+    div {
+      margin-top: 4vh;
+      display: flex;
+      flex-direction: row;
+      width: 60%;
+      justify-content: space-between;
 
-        input{
-          font-size: 1.3rem;
-          width: 70%;
-          height: 1.5rem;
-          text-align: center;
-          border: 1px solid black;
-        }
-
-        select{
-          width: 25%;
-          font-size: 1rem;
-          
-          option{
-            text-align: center;
-          }
-        }
+      input {
+        font-size: 1.3rem;
+        width: 70%;
+        height: 1.5rem;
+        text-align: center;
+        border-radius: 50px;
+        border: 1px solid ${theme.colors.primaryColor};
       }
 
-      button {
-            background-color: ${theme.colors.primaryColor};
-            color: ${theme.colors.textColor};
-            font-family: "minimal";
-            display: flex;
-            flex-wrap: wrap;
-            align-items: center;
-            justify-content: center;
-            text-decoration: none;
-            text-align: center;
-            border: none;
-            padding: 0 1rem;
-            box-sizing: border-box;
-            height: 3.8rem;
-            width: 15%;
-            font-size: 3vh;
-            border-radius: 3.125rem;
-            transition: all 0.5s;
-            
-            :hover {
-              cursor: pointer;
-              transform: scale(1.05);
-            }
-          }
+      select {
+        width: 25%;
+        font-size: 1rem;
+        border-radius: 50px;
+        border: 1px solid ${theme.colors.primaryColor};
+
+        option {
+          text-align: center;
+        }
+      }
+    }
+
+    button {
+      background-color: ${theme.colors.primaryColor};
+      color: ${theme.colors.textColor};
+      font-family: "minimal";
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: center;
+      text-decoration: none;
+      text-align: center;
+      border: none;
+      padding: 0 1rem;
+      box-sizing: border-box;
+      height: 3.8rem;
+      width: 15%;
+      font-size: 3vh;
+      border-radius: 3.125rem;
+      transition: all 0.5s;
+
+      :hover {
+        cursor: pointer;
+        transform: scale(0.99);
+        background-color: white;
+        border: 1px solid ${theme.colors.primaryColor};
+        color: ${theme.colors.primaryColor};
+        box-shadow: 3px 3px 8px ${theme.colors.primaryColorOpacity};
+      }
+    }
   `}
 `;
-
-
-
-
-
-
-
-
-
